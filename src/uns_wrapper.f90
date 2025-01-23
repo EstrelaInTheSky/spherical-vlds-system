40e3abfe79ba779c1dc72f3f02d2c3ec2007aa85
Amir Saadeddine <amir.saadeddine@ufv.br>
2025-01-23 20:07:51 -0300

    Add: Unsigned Integer Wrapper.

---
+++ src/uns_wrapper.f90
@@ -0,0 +1,28 @@
+module uns_wrapper
+    implicit none
+    ! Bits Quantity.
+    integer, parameter :: UINT_BIT = 32
+    ! 32-Bit Integer Derivate Type.
+    type :: uint32
+        integer(kind=4) :: value = 0
+    contains
+        procedure :: setUns   => set_uint32
+        procedure :: toDecimal  => to_decimal
+    end type uint32
+contains
+    ! Type Value Attribute.
+    subroutine set_uint32(CurrValue, NewValue)
+        class(uint32), intent(inout) :: CurrValue
+        integer, intent(in) :: NewValue
+
+        CurrValue%value = iand(NewValue, z'FFFFFFFF')
+    end subroutine set_uint32
+
+    ! Return Decimal Value.
+    function to_decimal(CurrValue) result(DecValue)
+        class(uint32), intent(in) :: CurrValue
+        integer :: DecValue
+
+        DecValue = iand(CurrValue%value, z'FFFFFFFF')
+    end function to_decimal
+end module uns_wrapper
